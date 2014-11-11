a0b5eb3f9e8ccafcc81f0888316b7ad30a65d4f3
Cory J Johnson <cory2014@gmail.com>
2014-11-11 14:56:25 -0600

    pc complete

--- ComputerArch/ComputerArch/Pc.h
+++ ComputerArch/ComputerArch/Pc.h
@@ -5,18 +5,20 @@ class Pc
 public:
 	Pc();
 	~Pc();
-	std::bitset<16>input;
-	std::bitset<16>output;
-	std::bitset<16>set();
+	
+	void set(std::bitset<16>input);
 	std::bitset<16>get();
 
 private:
+	std::bitset<16>input;
+	std::bitset<16>output;
 	std::bitset<16> current;
 	
 };
 
 Pc::Pc()
 {
+		current=std::bitset<16>(0);
 }
 
 Pc::~Pc()
