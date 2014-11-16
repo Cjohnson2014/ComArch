8b38c3c5da0466516ff598029af9448a7eddd65e
samsthompson <sam_s_thompson@aol.com>
2014-11-16 16:41:24 -0600

    Update IM

--- ComputerArch/ComputerArch/IM.cpp
+++ ComputerArch/ComputerArch/IM.cpp
@@ -12,12 +12,6 @@ IM::~IM()
     delete this;
 }
 
-IM IM::setIMWrite(bool IMWrite)
-{
-    this->IMWrite = IMWrite;
-    return *this;
-}
-
 IM IM::setIMRead(bool IMRead)
 {
     this->IMRead = IMRead;
@@ -28,17 +22,6 @@ IM IM::setIMAddress(std::bitset<16> IMaddress)
 {
     this->IMaddress = IMaddress;
     return *this;
-}
-
-IM IM::setWriteDataIM(std::bitset<16> writeDataIM)
-{
-    this->writeDataIM = writeDataIM;
-    return *this;
-}
-
-bool IM::getIMWrite()
-{
-    return this->IMWrite;
 }
 
 bool IM::getIMRead()
--- ComputerArch/ComputerArch/IM.h
+++ ComputerArch/ComputerArch/IM.h
@@ -10,25 +10,19 @@ class IM {
         IM();
         ~IM();
 
-        IM setIMWrite(bool IMWrite);
         IM setIMRead(bool IMRead);
 
         IM setIMAddress(std::bitset<16> IMaddress);
-        IM setWriteDataIM(std::bitset<16> writeDataIM);
 
-        bool getIMWrite();
         bool getIMRead();
 
         std::bitset<16> getReadDataIM();
 
     private:
 
-        bool IMWrite;
         bool IMRead;
 
         std::bitset<16> IMaddress;
-
-        std::bitset<16> writeDataIM;
 
         std::bitset<16> readDataIM;
 };
