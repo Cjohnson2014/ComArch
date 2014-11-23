38672e4a1c17d41889cf77d36570787d43f2e945
willharrison <jwh335@gmail.com>
2014-11-23 15:44:47 -0600

    bug fix

--- ComputerArch/ComputerArch/ComputerArch.cpp
+++ ComputerArch/ComputerArch/ComputerArch.cpp
@@ -8,24 +8,8 @@
 // our headers
 #include "Pc.h"
 
-// size of the machine code array
-#define ARRAY_SIZE 2
-
 // using standard namespace
 using namespace std;
-
-/**
- * Define all machine code
- */
-bitset<16> machinecode[ARRAY_SIZE] = {
-    bitset<16>(string("0101000001110001")),
-    bitset<16>(string("0111011010000111"))
-    /**
-     *
-     * The rest of the machine code will go here.
-     *
-     */
-};
 
 /**
  * Begin main execution
@@ -37,15 +21,9 @@ int main(int argc, char* argv[])
     cout << "arg count: " << argc << endl;
     cout << "arg vals: " << argv << endl;
 
-    for (int i = 0; i < ARRAY_SIZE; i++)
-    {
-		Pc Pc;
-		Pc.set(machinecode[i]);
-		cout << "Current Instruction"<< Pc.get() << "/n";
-
-        // read these files into memory.
-        // probably wont need a loop
-    }
+    Pc Pc;
+    Pc.set(0);
+    cout << "Current Instruction"<< Pc.get() << "/n";
 
     return 0;
 }
