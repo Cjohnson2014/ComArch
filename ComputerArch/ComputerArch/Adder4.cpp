41b360b519fddbf49db00f20e80a8783c7696fdf
willharrison <jwh335@gmail.com>
2014-11-23 15:32:09 -0600

    bug fix

--- ComputerArch/ComputerArch/Adder4.cpp
+++ ComputerArch/ComputerArch/Adder4.cpp
@@ -1,16 +1,16 @@
 #include "Adder4.h"
 
-Adder4::adder4()
+adder4::adder4()
 {
 	
 }
 
-Adder4::~adder4()
+adder4::~adder4()
 {
 	delete this;
 }
 
-std::bitset<16> Adder4::get()
+std::bitset<16> adder4::get()
 {
 	return this->addVal;
 }
